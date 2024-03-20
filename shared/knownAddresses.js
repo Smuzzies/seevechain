9503d57b0cff1de9ce438184cee9316814984c99
MicheleZetaDisplay <vetmaik@outlook.com>
2024-03-20 20:50:12 +0100

    Add vechain.energy contract

--- shared/knownAddresses.js
+++ shared/knownAddresses.js
@@ -1129,10 +1129,7 @@ const KNOWN_CONTRACTS = {
     short: 'VeSea',
     long: 'VeSea NFT: UkiDawgs',
    },
-  '0x83F158bbC757CE2fF61Ff5Ff119eCa7ad687a306': {
-    short: 'vechainenergy',
-    long: 'VTHO Swap Manager',
-   },
+  '0x83F158bbC757CE2fF61Ff5Ff119eCa7ad687a306': 'vechain.energy',
 }
 
     
@@ -1239,7 +1236,7 @@ const KNOWN_ADDRESSES = {
   '0x17b6254c7324438b469a01ce80b67dd7c4d5eef8': 'Plair ICO',
   '0x48e8dace6a1976d4912f8b5dcc3f45651c3d4b73': 'Safe Haven Boost',
   '0x27942b0d71919c4aa81b7ae6ba951150faef5ae6': 'VIP-191 Sponsor',
-  '0xFF5ba88a17b2E16D23FF6647E9052E937AcB1406': 'Coinbase HW4',
+  '0xFF5ba88a17b2E16D23FF6647E9052E937AcB1406': 'Coinbase (Hot Wallet 4)',
 }
 
 function getKnownContract(address, shortOrLong) {
