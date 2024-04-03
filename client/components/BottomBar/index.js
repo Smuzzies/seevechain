ad99f18187da0cc1f4b4774f47d48a77a6999034
TheSmuz <sunmanx@hexseven.com>
2024-04-03 18:47:38 +0000

    update css for bar

--- client/components/BottomBar/index.js
+++ client/components/BottomBar/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'preact/hooks'
-
 import useAppState from 'lib/appState'
 import numberWithCommas from 'lib/numberWithCommas'
-
 import './index.sass'
 
 export default function BottomBar({ togglePageModalVisibility }) {
@@ -20,31 +18,40 @@ export default function BottomBar({ togglePageModalVisibility }) {
         setNumberClass('BottomBar-number')
       }, 300)
     }
-
   }, [dailyTotals.dailyTransactions])
+
+  const navigateToTestnet = () => {
+    window.open('https://testnet.myvechain.com', '_blank')
+  }
 
   if (!dailyTotals || Object.keys(dailyTotals).length === 0) return <div className="BottomBar" />
 
-  return <div className="BottomBar" onClick={togglePageModalVisibility}>
-    <svg height="37" width="33" xmlns="http://www.w3.org/2000/svg" version="1.1">
-      <circle cx="16" cy="10" r="2" fill="grey" />
-      <circle cx="16" cy="18" r="2" fill="grey" />
-      <circle cx="16" cy="26" r="2" fill="grey" />
-    </svg>
-    <div className="BottomBar-wrapper">
-      <div>
-        <span className="BottomBar-header">Txs </span>
-        <span className={numberClass}>{numberWithCommas(dailyTotals.dailyTransactions) || 0}</span>
+  return (
+    <div className="BottomBar" onClick={togglePageModalVisibility}>
+      <div className="BottomBar-left">
+        <svg height="37" width="33" xmlns="http://www.w3.org/2000/svg" version="1.1">
+          <circle cx="16" cy="10" r="2" fill="grey" />
+          <circle cx="16" cy="18" r="2" fill="grey" />
+          <circle cx="16" cy="26" r="2" fill="grey" />
+        </svg>
       </div>
-      <div className="BottomBar-vtho">
-        <span className="BottomBar-header">{window.innerWidth < 400 ? '🔥' : 'VTHO Burn'} </span>
-        <span className={numberClass}>{numberWithCommas(Math.round(+dailyTotals.dailyVTHOBurn * 10) / 10) || 0}</span>
+      <div className="BottomBar-wrapper">
+        <div>
+          <span className="BottomBar-header">Txs </span>
+          <span className={numberClass}>{numberWithCommas(dailyTotals.dailyTransactions) || 0}</span>
+        </div>
+        <div className="BottomBar-vtho">
+          <span className="BottomBar-header">{window.innerWidth < 400 ? '🔥' : 'VTHO Burn'} </span>
+          <span className={numberClass}>{numberWithCommas(Math.round(+dailyTotals.dailyVTHOBurn * 10) / 10) || 0}</span>
+        </div>
+        <div>
+          <span className="BottomBar-header">Clauses </span>
+          <span className={numberClass}>{numberWithCommas(dailyTotals.dailyClauses) || 0}</span>
+        </div>
       </div>
-      <div>
-        <span className="BottomBar-header">Clauses </span>
-        <span className={numberClass}>{numberWithCommas(dailyTotals.dailyClauses) || 0}</span>
-      </div>
+      <button className="BottomBar-button" onClick={navigateToTestnet}>
+        Switch to TESTNET
+      </button>
     </div>
-  </div>
+  )
 }
-
